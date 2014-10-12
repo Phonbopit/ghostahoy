d1bffac5defec5da47ad2f6d19e09fee5f9c7119
Chai Phonbopit <phonbopit@gmail.com>
2014-10-12 16:41:22 +0700

    init

---
+++ assets/js/clean-blog.js
@@ -0,0 +1,40 @@
+/*!
+ * Clean Blog v1.0.0 (http://startbootstrap.com)
+ * Copyright 2014 Start Bootstrap
+ * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
+ */
+(function ($) {
+  "use strict";
+
+  var MQL = 1170;
+
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $('.navbar-custom').height();
+    $(window).on('scroll', {
+        previousTop: 0
+    },
+    function() {
+      var currentTop = $(window).scrollTop();
+      //check if user is scrolling up
+      if (currentTop < this.previousTop) {
+        //if scrolling up...
+        if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+          $('.navbar-custom').addClass('is-visible');
+        } else {
+          $('.navbar-custom').removeClass('is-visible is-fixed');
+        }
+      } else {
+        //if scrolling down...
+        $('.navbar-custom').removeClass('is-visible');
+        if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+      }
+        this.previousTop = currentTop;
+    });
+  }
+
+  //hightlight.js
+  $("pre code").each(function(i, e) {hljs.highlightBlock(e); });
+
+}(jQuery));
+
